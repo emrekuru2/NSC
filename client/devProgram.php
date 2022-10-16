14503a127a8d6c78c67661296ebdc70141e259b4
Suhani Tayal <sh362154@dal.ca>
2022-10-16 14:48:17 -0300

    Banner and Text(on Banner)

---
+++ client/devProgram.php
@@ -0,0 +1,17 @@
+
+<div class="banner">
+    <img class = "banner_img" src='./img/dummyImg/banner_DevProgram.jpg' alt='banner picture'>
+    <div class= "banner_text">
+        <h2>Dummy Text</h2>
+    </div>
+</div>
+
+<div class= "random"></div>
+
+<link rel="stylesheet" type="text/css" href="../../css/devProgram.css">
+<!-- issue -->
+</footer>
+    <?php
+    include 'includes/components/footer.php';
+    ?>
+</footer>
