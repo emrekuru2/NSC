f187b37e3f2d46c54a41ee6b98fcd27e97b06f49
Hao Tang <61295938+haotangphuoc@users.noreply.github.com>
2022-10-17 16:10:04 -0300

    Add the programns section belowbanner

--- client/devProgram.php
+++ client/devProgram.php
@@ -1,3 +1,9 @@
+<?php
+$title = "News";
+include 'includes/components/header.php';
+?>
+
+<link rel="stylesheet" type="text/css" href="../../css/devProgram.css">
 
 <div class="banner">
     <img class = "banner_img" src='./img/dummyImg/banner_DevProgram.jpg' alt='banner picture'>
@@ -6,12 +12,36 @@
     </div>
 </div>
 
-<div class= "random"></div>
 
-<link rel="stylesheet" type="text/css" href="../../css/devProgram.css">
+<div class="programs-board">
+    <div class="program" id="program1">
+        <img class="program-img" src="./img/dummyImg/program-pic1.jpeg" alt="program picture">
+        <div class="program-info">
+            <h2>Dummy text</h2>
+            <button class="info-button" onclick="#">More info!</button>
+        </div>
+    </div>
+
+    <div class="program" id="program2">
+        <img class="program-img" src="./img/dummyImg/program-pic2.jpeg" alt="program picture">
+        <div class="program-info">
+            <h2>Dummy text</h2>
+            <button class="info-button" onclick="#">More info!</button>
+        </div>
+    </div>
+
+    <div class="program" id="program3">
+        <img class="program-img" src="./img/dummyImg/program-pic3.jpeg" alt="program picture">
+        <div class="program-info">
+            <h2>Dummy text</h2>
+            <button class="info-button" onclick="#">More info!</button>
+        </div>
+    </div>
+</div>
+
 <!-- issue -->
-</footer>
-    <?php
+
+<?php
     include 'includes/components/footer.php';
-    ?>
-</footer>
+?>
+
