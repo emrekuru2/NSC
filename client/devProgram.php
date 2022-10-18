ceeaa52483b160d092bcdce3e114b57ee706d229
Songbo Niu <sn629057@da.ca>
2022-10-18 07:57:57 -0300

    sticky to the bottom

--- client/devProgram.php
+++ client/devProgram.php
@@ -5,43 +5,49 @@ include 'includes/components/header.php';
 
 <link rel="stylesheet" type="text/css" href="../../css/devProgram.css">
 
-<div class="banner">
-    <img class = "banner_img" src='./img/dummyImg/banner_DevProgram.jpg' alt='banner picture'>
-    <div class= "banner_text">
-        <h2>Dummy Text</h2>
-    </div>
-</div>
+<body>
+    <div class = "content">
+        <div class = "content-inside">
+            <div class="banner">
+                <img class = "banner_img" src='./img/dummyImg/banner_DevProgram.jpg' alt='banner picture'>
+                <div class= "banner_text">
+                    <h2>Dummy Text</h2>
+                </div>
+            </div>
 
 
-<div class="programs-board">
-    <div class="program" id="program1">
-        <img class="program-img" src="./img/dummyImg/program-pic1.jpeg" alt="program picture">
-        <div class="program-info">
-            <h2>Dummy text</h2>
-            <button class="info-button" onclick="#">More info!</button>
+            <div class="programs-board">
+                <div class="program" id="program1">
+                    <img class="program-img" src="./img/dummyImg/program-pic1.jpeg" alt="program picture">
+                    <div class="program-info">
+                        <h2>Dummy text</h2>
+                        <button class="info-button" onclick="#">More info!</button>
+                    </div>
+                </div>
+
+                <div class="program" id="program2">
+                    <img class="program-img" src="./img/dummyImg/program-pic2.jpeg" alt="program picture">
+                    <div class="program-info">
+                        <h2>Dummy text</h2>
+                        <button class="info-button" onclick="#">More info!</button>
+                    </div>
+                </div>
+
+                <div class="program" id="program3">
+                    <img class="program-img" src="./img/dummyImg/program-pic3.jpeg" alt="program picture">
+                    <div class="program-info">
+                        <h2>Dummy text</h2>
+                        <button class="info-button" onclick="#">More info!</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
-    <div class="program" id="program2">
-        <img class="program-img" src="./img/dummyImg/program-pic2.jpeg" alt="program picture">
-        <div class="program-info">
-            <h2>Dummy text</h2>
-            <button class="info-button" onclick="#">More info!</button>
-        </div>
-    </div>
+    <!-- issue -->
 
-    <div class="program" id="program3">
-        <img class="program-img" src="./img/dummyImg/program-pic3.jpeg" alt="program picture">
-        <div class="program-info">
-            <h2>Dummy text</h2>
-            <button class="info-button" onclick="#">More info!</button>
-        </div>
-    </div>
-</div>
-
-<!-- issue -->
-
-<?php
-    include 'includes/components/footer.php';
-?>
+    <?php
+        include 'includes/components/footer.php';
+    ?>
+</body>
 
