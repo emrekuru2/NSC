66f181fcef47778d2a7c09135141f29f8a52717a
Suhani Tayal <sh362154@dal.ca>
2022-11-05 18:28:41 -0300

    search-bar

--- client/manageTeam.php
+++ client/manageTeam.php
@@ -5,17 +5,20 @@ include 'includes/components/header.php';
 
 
 <link rel="stylesheet"  href="../../css/manageTeam.css">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 
 <body>
-
-    <div class = "search-bar">
-    <form class="" role="search">
-        <input class="" type="search" placeholder="Search">
-        <button class="" type="submit">Search</button>
-      </form>
+    <div class="search-bar">
+        <form class="form-inline my-2 my-lg-0 d-flex justify-content-center" id= "search-form">
+            <input class="form-control" style="width: 75%" type="search" placeholder="Search Player" aria-label="Search">
+            <button class="btn btn-md" type="submit" id="search-button">Search</button>
+        </form>
     </div>
 
-    <div class = "team lsit">
+    <div class = "team list">
         <div class = "team">
             <div class = "buttons">
                     <button type="button" class="">Add</button>
@@ -57,7 +60,8 @@ include 'includes/components/header.php';
 
 
 
-    <?php
-        include 'includes/components/footer.php';
-    ?>
 </body>
+
+<?php
+        include 'includes/components/footer.php';
+?>
