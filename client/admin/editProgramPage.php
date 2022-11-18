1a3d6c3f23472edc5779cde9446ccde1c2c3e05a
Shreya-V-Sharma <shreyasharmacanada@gmail.com>
2022-11-17 20:18:04 -0400

    Edited the form inside the editProgramPage to add new development program in the admin dashboard

--- client/admin/editProgramPage.php
+++ client/admin/editProgramPage.php
@@ -26,25 +26,44 @@
                     <p class="h4 mb-4">Create A New Program</p>
                     <br><br>
                     <!-- Name -->
-                    <input type="text"  name="programName" class="form-control mb-4" placeholder="Name of the program">
+                    <div class="form-group row">
+                        <label for="programName" class="col-form-label">Program Name</label>
+                        <input type="text"  name="programName" class="form-control mb-4" placeholder="Ex.Youth Summer Camp" required>
+                    </div>
 
                     <!-- Duration -->
-                    <input type="text"  name="duration" class="form-control mb-4" placeholder="Duration of the program. Ex. 16 weeks">
+                    <div class="form-group row">
+                        <label for="duration" class="col-form-label">Program Duration</label>
+                        <input type="text"  name="duration" class="form-control mb-4" placeholder="Ex. 16 weeks" required>
+                    </div>
 
                     <!-- Discription -->
-                    <textarea name="programDescription"  placeholder="Description of the prorgam" class="form-control mb-4" ></textarea>
-
+                    <div class="form-group row">
+                        <label for="programDescription" class="col-form-label">Program Discription</label>
+                        <textarea name="programDescription"  placeholder="Ex. Fun summer camp aiming to teach cricket to youth" class="form-control mb-4" required></textarea>
+                    </div>
                     <!-- Time -->
-                    <input type="text"  name="time" class="form-control mb-4" placeholder="Time of the program. Ex. 0915-1515">    
-                    
+                    <div class="form-group row">
+                        <label for="time" class="col-form-label">Program Timing</label>
+                        <input type="text"  name="time" class="form-control mb-4" placeholder="Ex. 0915-1515" required>    
+                    </div>
                     <!-- Charges -->
-                    <input type="text"  name="charges" class="form-control mb-4" placeholder="Charges. Ex. $50 monthly">
+                    <div class="form-group row">
+                        <label for="charge" class="col-form-label">Charge for the Program</label>
+                        <input type="text"  name="charges" class="form-control mb-4" placeholder="Ex. $50 monthly" required>
+                    </div>
 
                     <!-- Type -->
-                    <input type="text"  name="type" class="form-control mb-4" placeholder="Type of the program. Ex. youth">
+                    <div class="form-group row">
+                        <label for="type" class="col-form-label">Program Type</label>
+                        <input type="text"  name="type" class="form-control mb-4" placeholder="Ex. youth" required>
+                    </div>
 
                     <!-- Days run -->
-                    <input type="text"  name="days" class="form-control mb-4" placeholder="Days of the week the program is run. Ex. Saturdays and Sundays">
+                    <div class="form-group row">
+                        <label for="days" class="col-form-label">Days of the week Program is run</label>
+                        <input type="text"  name="days" class="form-control mb-4" placeholder="Ex. Saturdays and Sundays" required>
+                    </div>
 
                     <!-- Sign in button -->
                     <button class="btn btn-info btn-block my-4" type="submit">Submit</button>
