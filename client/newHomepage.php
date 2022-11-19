98a2896abb6bb93d5d41a0506ada723f806340bc
Songbo Niu <sn629057@dal.ca>
2022-11-19 04:12:08 -0400

    Initial new footer header and homepage

---
+++ client/newHomepage.php
@@ -0,0 +1,22 @@
+<?php
+    $title = "newHomepage";
+    include 'includes/components/newheader.php';
+
+    $conn = OpenCon();
+?>
+<div class="container">
+  
+    <div class="col mx-auto" style="text-align:center">
+        <h1 class="about" style="text-align:center;">About</h1>
+        <p style="text-align:justify">some content some content some content some content some content some content some content some content some content</p>
+    </div>
+    
+</div>
+    
+
+</div>
+
+<?php
+include "includes/components/newfooter.php";
+
+?>
