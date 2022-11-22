f2d5cd21a5d6a4ee7ebe00d5e60620e0d0bf15fd
Hao Tang <61295938+haotangphuoc@users.noreply.github.com>
2022-11-22 04:31:09 -0400

    Adding team section

--- client/newHomepage.php
+++ client/newHomepage.php
@@ -4,7 +4,7 @@ include 'includes/components/newheader.php';
 <head>
     <title>Home</title>
 </head>
-<div >
+
     
 
 <!-- The about us section:
@@ -67,36 +67,49 @@ include 'includes/components/newheader.php';
     </div>
     
     <!-- The New Section -->
-    <div class="d-flex flex-row justify-content-between" style="padding-top:2vw;">
-        <div class="card" style="width: 22vw; height: 30vw">
+    <div class="container d-flex flex-row justify-content-between" style="padding-top:2vw;">
+        <div class="card" style="width: 20vw; height: 30vw">
 
             <div class="card-body text-center">
                 <h5 class="card-title ">News title</h5>
                 <p class="card-text m-4 ">News content</p>
-                <a href="#" class="btn btn-primary position-absolute bottom-0 start-50 translate-middle-x">Find out more</a>
+                <a href="#" class="btn btn-primary position-absolute bottom-4vw start-50 translate-middle-x">Find out more</a>
             </div>
         </div>
 
-        <div class="card" style="width: 22vw;">
+        <div class="card" style="width: 20vw;">
             <div class="card-body">
                 <img src="img/teamProfilePictures/HomePageNews1.jpeg" alt="New image" style="width:100%;height:12vw;">
                 <p class="card-text m-4 text-center">News content</p>
             </div>
         </div>
 
-        <div class="card" style="width: 22vw;">
+        <div class="card" style="width: 20vw;">
             <div class="card-body">
                 <img src="img/teamProfilePictures/HomePageNews2.jpeg" alt="New image" style="width:100%;height:12vw;">
                 <p class="card-text m-4 text-center">News content</p>
             </div>
         </div>
 
-        <div class="card " style="width: 22vw;">
+        <div class="card " style="width: 20vw;">
             <div class="card-body">
                 <img src="img/teamProfilePictures/HomePageNews3.jpeg" alt="New image" style="width:100%;height:12vw;">
                 <p class="card-text m-4 text-center">News content</p>
             </div>
         </div>
-
     </div>
-</div>
+
+    <!-- The Team Section -->
+    <div class="container d-flex flex-row justify-content-between" style="padding-top:2vw;">
+        <div style="padding:4vw">
+            <h3>Team</h3>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.  </p>
+            <a href="#" class="btn btn-primary">Find out more</a>
+        </div>
+        <div>
+            <img src="img/teamProfilePictures/HomePageTeam.jpeg" alt="Homme Page Team"  style="width:40vw">
+        </div>
+    </div>
+
+
+
