b3272f89b38a907eac505519e0f528b9d76e3bff
Suhani Tayal <sh362154@dal.ca>
2022-11-22 10:45:42 -0400

    Re-doing the footer

--- client/includes/components/newfooter.php
+++ client/includes/components/newfooter.php
@@ -1,89 +1,41 @@
 <!-- learning from https://mdbootstrap.com/docs/standard/navigation/footer/ -->
-<footer class=" text-center text-lg-start text-white bg-primary mt-5">
-    <div class="container p-4 bg-primary" >
-        <div class="row">
-            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+<footer class=" text-center text-lg-start text-white bg-primary" style="height:35vh;">
+    <div class="container" >
+        <div class="row pt-4">
+            <div class="col-lg-3 col-6 p-2 bg-info">
                 <h5 class="text-uppercase">Contact us</h5>
 
                 <ul class="list-unstyled mb-0">
                     <li>
-                        <a href="https://www.facebook.com/novascotiacricket/" class="mr-3 text-white fs-2" role="button"><i class="bi bi-facebook"></i></a>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-2">
+                                <a href="https://www.facebook.com/novascotiacricket/" class="mr-3 text-white fs-2" role="button"><i class="bi bi-facebook"></i></a>
+                                </div>
+                                <div class="col m-auto"><h5>Facebook</h5></div>
+                            </div>
+                        </div>
                     </li>
                     <li>
-                        <a href="https://twitter.com/nscricket" class="mr-3 text-white fs-2" role="button"><i class="bi bi-twitter"></i></a>
-                    </li>
-                    <li>
-                        <a href="http://www.novascotiacricket.com" class="mr-3 text-white fs-2" role="button"><i class="bi bi-browser-edge"></i></i></a>
-                    </li>
-                </ul>
-            </div>
-            
-            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h5 class="text-uppercase">some topics</h5>
-
-                <ul class="list-unstyled">
-                    <li>
-                        <a href="#!" class="text-white text-decoration-none">somehting</a>
-                    </li>
-                    <li>
-                        <a href="#!" class="text-white text-decoration-none">somehting</a>
-                    </li>
-                    <li>
-                        <a href="#!" class="text-white text-decoration-none">somehting</a>
-                    </li>
-                    <li>
-                        <a href="#!" class="text-white text-decoration-none">somehting</a>
-                    </li>
-                </ul>
-            </div>
-        
-        
-            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                    <h5 class="text-uppercase">some topic</h5>
-
-                    <ul class="list-unstyled">
-                    <li>
-                        <a href="#!" class="text-white text-decoration-none">somehting</a>
-                    </li>
-                    <li>
-                        <a href="#!" class="text-white text-decoration-none">somehting</a>
-                    </li>
-                    <li>
-                        <a href="#!" class="text-white text-decoration-none">somehting</a>
-                    </li>
-                    <li>
-                        <a href="#!" class="text-white text-decoration-none">somehting</a>
-                    </li>
-                    </ul>
-            </div>
-        
-            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h5 class="text-uppercase">Some topic</h5>
-
-                <ul class="list-unstyled">
-                    <li>
-                        <a href="#!" class="text-white text-decoration-none">policy</a>
-                    </li>
-                    <li>
-                        <a href="#!" class="text-white text-decoration-none">somehting</a>
-                    </li>
-                    <li>
-                        <a href="#!" class="text-white text-decoration-none">somehting</a>
-                    </li>
-                    <li>
-                        <a href="#!" class="text-white text-decoration-none">somehting</a>
+                    <div class="container">
+                            <div class="row">
+                                <div class="col-2">
+                                <a href="http://www.novascotiacricket.com" class="mr-3 text-white fs-2" role="button"><i class="bi bi-envelope"></i></a>
+                                </div>
+                                <div class="col m-auto"><h5>Email</h5></div>
+                            </div>
+                        </div>
+                        
                     </li>
                 </ul>
             </div>
        
         </div>
     
-    </div>
-   
-    <div class="text-center p-3 bg-primary fs-4" >
+        <div class="text-center pb-3 fs-7" >
         © 2020 Copyright:
-        <a class="text-white fs-4" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        <a class="text-white fs-7" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        </div>
     </div>
-  
-</body>
+</footer>
 </html>
