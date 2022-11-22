aaa7688ee2c6928721d1bf05759f4dee63542555
Suhani Tayal <sh362154@dal.ca>
2022-11-22 00:17:51 -0400

    Fixed New Header

--- client/includes/components/newheader.php
+++ client/includes/components/newheader.php
@@ -1,195 +1,65 @@
-<?php
-    // Prevent direct access
-    // If it the admin then all the validation is already handled in admin files.
-    if(preg_match("/admin/", $_SERVER["PHP_SELF"]) == 0) {
-        if(preg_match("/includes/", $_SERVER["PHP_SELF"]) == 1) {
-            header("Location: ../../index.php");
-            die();
-        }
-        else {
-            include_once("includes/functions/security.php");
-            RestrictIncludes();
-            DefineSecurity();
-        }
-
-        include_once("db/database.php");
-        include_once("db/dbFunctions.php");
-        $conn = OpenCon();
-    }
-
-    if (isset($loginOrRegistrationPage) && $loginOrRegistrationPage) 
-    {
-        session_start();
-        $session_token = md5(uniqid(rand(), true));
-
-        $_SESSION['session_token'] = $session_token;
-    }
-    else 
-    {
-        if(session_status() !== PHP_SESSION_ACTIVE) session_start();
-    }
-    
-?>
-
-
 <!doctype html>
 <html lang="en">
-
-<head>
-    <base href="header.php">
+  <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <title><?php echo $title ?></title>
-
-    <link rel="icon" href="../../img/favicon.jpeg">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" type="text/css" href="../../css/bootstrap.css">
-    <!-- Material Design Bootstrap -->
-    <link rel="stylesheet" type="text/css" href="../../css/mdb.min.css">
-    <!-- Your custom styles (optional) -->
-    <link rel="stylesheet" type="text/css" href="../../css/style.css">
-</head>
-
-<?php  
-    if (!isset($loginOrRegistrationPage)) {
-?>
-
-<body>
-<img src="img/headerImg.png" alt="logo">
-<nav class="navbar navbar-expand-lg navbar-dark light-blue">
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="../../">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="../../Club.php">Clubs</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="../../Committees.php">Committees</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="../../TeamPage.php">Teams</a>
-            </li>
-            <?php
-            if (isset($_SESSION['LoggedIn']) && $_SESSION['LoggedIn'] == true) {
-                 if (isset($_SESSION['User_ID']) && CheckRole($_SESSION['User_ID']) == 'Manager') {
-            ?>
-                <li class = "nav-item">
-                    <a class = "nav-link" href="../../ClubProfile.php">My Club</a>
-                </li>
-                <li class = "nav-item">
-                    <a class = "nav-link" href="../../manageTeam.php">Manage Team</a>
-                </li>
-                <?php
-                }
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>header</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
+  </head>
+  <body>
     
-            }
-            ?>
-            <?php
-            if (isset($_SESSION['LoggedIn']) && $_SESSION['LoggedIn'] == true) {
-                if (isset($_SESSION['User_ID']) && (CheckRole($_SESSION['User_ID']) == 'Player')) {
-            ?>
-                   <li class = "nav- item">
-                       <a class = "nav-link" href = "../../PlayerProfile.php">Player Profile</a>
-                </li>
-                <?php
-                }
-            }
-            ?>
-            <?php
-            if (isset($_SESSION['LoggedIn']) && $_SESSION['LoggedIn'] == true) {
-                 if (isset($_SESSION['User_ID']) && (CheckRole($_SESSION['User_ID']) == 'Coach' || CheckRole($_SESSION['User_ID']) == 'Player')) {
-            ?>
-                <li class = "nav-item">
-                    <a class = "nav-link" href="../../Team.php">My Team</a>
-                </li>
-                <?php
-                }
-    
-            }
-            ?>
-
-            <?php if (isset($_SESSION['LoggedIn']) && $_SESSION['LoggedIn'] == true) { ?>
-            <li class="nav-item">
-                <a class="nav-link" href="../../devProgram.php">Development Programs</a>
-            </li>
-            <?php } ?>
-
-        </ul>
-        <ul class="navbar-nav ml-auto">
-
-            <!-- Join a team or My Team -->
-            <?php
-                checkHasATeam();
-            ?>
-
-            <li class="nav-item">
-                <a class="nav-link waves-effect waves-light" href="../../news.php">
-                    <i class="fas fa-envelope"></i> News
-                </a>
-            </li>
-
-
-            <?php
-
-            if (isset($_SESSION['LoggedIn']) && $_SESSION['LoggedIn'] == true) { ?>
-
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id=\"navbarDropdownMenuLink-333\" data-toggle="dropdown"
-                   aria-haspopup="true" aria-expanded="false">
-                    <i class="fas fa-user"></i> <?php echo $_SESSION['FirstName']." ".$_SESSION['LastName']?> </a>
-                <div class="dropdown-menu dropdown-menu-right dropdown-default\" aria-labelledby=\"navbarDropdownMenuLink-333\">
-                    <a class="dropdown-item" href="../../UserProfile.php">My Profile</a>
-                    <div class="dropdown-divider"></div>
-                    <?php
-                        if (isset($_SESSION['User_ID']) && CheckRole($_SESSION['User_ID']) == 'Admin') {
-                            ?>
-                            <a class="dropdown-item" href="../../admin/dashboard.php">Admin Dashboard</a>
-                            <?php
-                        }
-                        if(preg_match("/admin/", $_SERVER["PHP_SELF"]) == 1) {
-                            echo  '<a class="dropdown-item" href="../includes/components/logout.php">Logout</a>';
-                        }
-                        else {
-                            echo '<a class="dropdown-item" href="includes/components/logout.php">Logout</a>';
-                        }
-                    ?>
+    <!-- Logo, bell and login -->
+    <nav class="navbar bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <img src="img/headerImg.png" alt="logo" width="30" height="24">
+                        </div>
+                        <div class="col" style="color: rgb(85, 128, 207);">
+                            <h4>
+                            Nova Scotia <br>
+                            Cricket Association
+                            </h4>
+                        </div>
+                    </div>
                 </div>
-            </li>
-            <?php
-            } else {
-                ?>
-                <li class="nav-item">
-                <a class="nav-link waves-effect waves-light" href="loginform.php">
-                    <i class="fas fa-user"></i> Login
+            </a>
+            <div class="d-flex bell-and-login ">
+                <a class="nav-link ml-auto p-4" href="../../news.php">
+                    <i class="bi bi-bell" style="color: cornflowerblue; font-size: 5vh; "></i>
                 </a>
-                </li>
-        <?php
-            }
-            ?>
-        </ul>
-    </div>
-</nav>
-
-<?php
-        $conn = OpenCon();
-        if (getAlertStatus($conn) == "active"){
-            $row = getAlerts($conn);
-            echo mysqli_error($conn);
-            ?>
-            <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                <h4 class="alert-heading"><?php echo $row['Alert_Title']; ?></h4>
-                <p><?php echo $row['Alert_Content']; ?></p>
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+                <a class="nav-link ml-auto p-4" href="../../loginform.php">
+                        <i class="bi bi-person-circle" style="color: cornflowerblue; font-size: 5vh;"></i> Login
+                </a>
             </div>
 
-            <?php
-        }
+        </div>
+    </nav>
 
-    }
-?>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg bg-primary " >
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav" style="width:50vw; margin-left:2vw; font-size: 1.5vw; ">
+                    <li class="nav-item">
+                    <a class="nav-link text-white " href="../../">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link text-white" href="../../Club.php">Clubs</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link text-white" href="../../Committees.php">Committees</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link text-white" href="../../TeamPage.php">Teams</a>
+                    </li>
+                </ul>
+            </div>
+    </nav>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    
+  </body>
+</html>
--- client/newHomepage.php
+++ client/newHomepage.php
@@ -1,49 +1,11 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
-  </head>
-  <body>
-    <nav class="navbar bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">
-                <img src="img/headerImg.png" alt="logo" width="30" height="24">
-            </a>
-                <a class="nav-link waves-effect waves-light" href="../../news.php">
-                    <i class="bi bi-bell" style="color: cornflowerblue; "></i>
-                </a>
-                <a class="nav-link waves-effect waves-light" href="loginform.php">
-                        <i class="bi bi-person-circle" style="color: cornflowerblue; "></i> Login
-                </a>
+<?php
+include 'includes/components/newheader.php';
+?>
+<head>
+    <title>Home</title>
+</head>
 
-        </div>
-    </nav>
-    <nav class="navbar navbar-expand-lg p-3 mb-2 bg-primary" >
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav" style="width:50vw; margin-left:35vw; text-align:center">
-                    <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Club</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Committees</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Teams</a>
-                    </li>
-                </ul>
-            </div>
-    </nav>
-
-  
-  
-    <div class="block1 " style="position:relative; width:50vw; margin-left:30vw">
+<div class="block1 " style="position:relative; width:50vw; margin-left:30vw">
         <img src="img/background.jpeg" alt="about-us">
         <div class="bg-success p-2 text-dark bg-opacity-25" style="position:absolute; z-index:2; left:10px; top:10px;">
             <h3>About us</h3>
@@ -54,8 +16,4 @@
             
 
         </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    
-  </body>
-</html>
+</div>
