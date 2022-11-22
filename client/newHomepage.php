6ed047ac47bd35c8b53f7b71838b6baa4df59107
Suhani Tayal <sh362154@dal.ca>
2022-11-22 03:14:04 -0400

    DevProgram Overflow sloved

--- client/newHomepage.php
+++ client/newHomepage.php
@@ -20,52 +20,50 @@ include 'includes/components/newheader.php';
         </div>      
     </div>
 
-    <div class="container pl-0 ml-0 pr-0 mr-0" style="padding-top:2vw;">
-        <div class="row">
-            <div class="col-8">
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" >
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner" >
-                    <div class="carousel-item active" style="height: 15rem;width: 130vh;">
-                        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"  preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
-                        <div class="carousel-caption text-end">
-                            <h1>Clubs</h1>
-                            <p>Take a look at the our clubs that play in the HCL</p>
-                            <p><a class="btn btn-primary" href="Club.php">Find out more</a></p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                    <img class="d-block" src="../../img/clubs/EastCoastCricketClub.jpg" alt="Second slide">
-                    </div>
-                    <div class="carousel-item">
-                    <img class="d-block w-100" src="..." alt="Third slide">
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+    <div class="container d-flex" style="padding-top:2vw;">
+        
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" >
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" >
+            <div class="carousel-item active" style="height: 15rem;width: 130vh;">
+                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"  preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+                <div class="carousel-caption text-end">
+                    <h1>Clubs</h1>
+                    <p>Take a look at the our clubs that play in the HCL</p>
+                    <p><a class="btn btn-primary" href="Club.php">Find out more</a></p>
                 </div>
             </div>
-            <div class="col-4 pl-0 pr-0">
-                <div class="card bg-info " style="height:15rem; width: 70vh;">
-                    <div class="card-body text-center pt-5" style="color: white;" >
-                        <h2 class="card-title">Development Programs</h2>
-                        <p class="card-text " style="color: white;">Sign up for our new and upcoming programs
-                            <br>and become a member at NSCA</p>
-                            <p><a class="btn btn-primary" href="devProgram.php">Get involved</a></p>
-                    </div>
-                </div>
+            <div class="carousel-item">
+            <img class="d-block" src="../../img/clubs/EastCoastCricketClub.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block w-100" src="..." alt="Third slide">
             </div>
         </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        </div>
+
+        <!-- DevPrograms Card -->
+        <div class="card bg-info " style="height:15rem; width: 70vh;">
+            <div class="card-body text-center pt-5" style="color: white;" >
+                <h2 class="card-title">Development Programs</h2>
+                <p class="card-text " style="color: white;">Sign up for our new and upcoming programs
+                    <br>and become a member at NSCA</p>
+                    <p><a class="btn btn-primary" href="devProgram.php">Get involved</a></p>
+            </div>
+        </div>
+                 
     </div>
     
     <!-- The New Section -->
