80183bc268fdb906a5286094927f0c173dd9f6ad
Hao Tang <61295938+haotangphuoc@users.noreply.github.com>
2022-11-30 01:29:59 -0400

    Fixing styling for the Clubs page

--- client/Club.php
+++ client/Club.php
@@ -16,7 +16,7 @@
     <section class="mx-md-5 dark-grey-text">
 
         <!-- Section heading -->
-        <h3 class="text-center font-weight-bold mb-4 pb-2">HCL CLUBS</h3>
+        <h1 class="text-center font-weight-bold mb-4 pb-2">HCL CLUBS</h3>
         <!-- Section description -->
         <p class="text-center mx-auto mb-5"> The following clubs play in the Halifax Cricket League (HCL) outdoor summer competition.
             <br>If you wish to play outdoor summer cricket, please contact the following clubs.</p>
@@ -33,7 +33,7 @@
 
             ?>
             <!-- Post title -->
-            <h4 class="font-weight-bold mb-3"><strong><?=$row['Name']?></strong></h4>
+            <h2 class="font-weight-bold mb-3 text-center"><strong><?=$row['Name']?></strong></h4>
 
             <div class="club-container">
                 <img class="feature-img rounded-circle" src="<?=$row['TeamImage']?>"
@@ -41,24 +41,24 @@
                 
                   
                 <div class="club-info">
-                    <div class="email">
+                    <div class="info-container">
                         <i class="fa-solid fa-envelope"></i>
-                        <a href="Mailto:<?=$row['Email']?>"><?=$row['Email']?></a><br>
+                        <a href="Mailto:<?=$row['Email']?>"><?=$row['Email']?></a>
                     </div>
 
-                    <div class="phone">
+                    <div class="info-container">
                         <i class="fa-solid fa-phone"></i>
-                        <a href="tel:<?=$row['Phone']?>"><?=$row['Phone']?></a><br>
+                        <a href="tel:<?=$row['Phone']?>"><?=$row['Phone']?></a>
                     </div>
 
-                    <div class="facebook">
+                    <div class="info-container">
                         <i class="fa-brands fa-facebook"></i>
-                        <a href="<?=$row['Facebook']?>"><?=$row['Name']?></a><br>
+                        <a href="<?=$row['Facebook']?>"><?=$row['Name']?></a>
                     </div>
                     <?php
                         if (!empty($row['Website'])) {
                             ?>
-                            <div>
+                            <div class = "info-container">
                                 <i class="fa-solid fa-window-maximize"></i>
                                 <a href="<?=$row['Website']?>"><?=$row['Website']?></a>
                             </div>
@@ -67,7 +67,7 @@
                     ?>
                 </div>
             </div>
-            <br><br><br>
+            <br><br><br><br><br>
             <?php
         }
         ?>
