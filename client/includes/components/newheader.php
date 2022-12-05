b98e397080a8891673894cdf4b96af3463f2ae74
monjur <mh368118@dal.ca>
2022-12-05 19:07:52 -0400

    Fixed merge issues in newHeader.php

--- client/includes/components/newheader.php
+++ client/includes/components/newheader.php
@@ -83,6 +83,9 @@
             <li class="nav-item">
                 <a class="nav-link" href="../../TeamPage.php">Teams</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../../devProgram.php">Development Programs</a>
+            </li>
             <?php
             if (isset($_SESSION['LoggedIn']) && $_SESSION['LoggedIn'] == true) {
                  if (isset($_SESSION['User_ID']) && CheckRole($_SESSION['User_ID']) == 'Manager' || CheckRole($_SESSION['User_ID']) == 'Admin') {
@@ -119,12 +122,6 @@
     
             }
             ?>
-
-            <?php if (isset($_SESSION['LoggedIn']) && $_SESSION['LoggedIn'] == true) { ?>
-            <li class="nav-item">
-                <a class="nav-link" href="../../devProgram.php">Development Programs</a>
-            </li>
-            <?php } ?>
 
         </ul>
         <ul class="navbar-nav ml-auto" style="font-size:1.3vw;">
