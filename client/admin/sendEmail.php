19243c9730b3acbcffcc9d5a996a63137b6cd6d1
macnutt <nc495758@dal.ca>
2023-02-03 19:16:46 -0400

    Started refactoring Admin Send Email page.

--- client/admin/sendEmail.php
+++ client/admin/sendEmail.php
@@ -62,64 +62,44 @@
     }
 ?>
 
-    <div class="container-fluid">
+    <form class="container-fluid" action="sendEmail.php" method="POST">
 
-        <div class="row">
-            <div class="col-7 offset-2">
-                <div class="text-center">
-                    <h1 class="h1 mb-0 text-gray-800">Send Email</h1>
-                </div>
-                <!-- Default form login -->
-
-            </div>
+        <!-- Title -->
+        <div class="send-email-row">
+            <h1 class="send-email-title h1 mb-0 text-gray-800">Send Email</h1>
         </div>
-        <div class="row">
-            <div class="col-7 offset-2">
 
-                <form class="text-center" action="sendEmail.php" method="POST">
-
-
-                    <div class="form-group row">
-                        <label for="emailFormTitle" class="col-form-label">Email Subject</label>
-                        <input type="text" id="emailFormTitle" name="emailFormTitle" class="form-control"
-                               placeholder="Subject" required>
-                    </div>
-
-                    <div class="form-group row">
-                        <label for="emailFormFirstName" class="col-form-label">Full Name</label>
-                        <input type="text" id="emailFormFullName" name="emailFormFullName" class="form-control"
-                               placeholder="Full Name">
-                        <div class="mt-1">
-                            <i class="fas fa-exclamation-circle" style="color:orange"></i> Leave this blank if you do
-                            not want your name to be displayed as the sender.
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label for="emailFormEmail" class="col-form-label">Recipient Email</label>
-                        <input type="email" id="emailFormEmail" name="emailFormEmail" class="form-control"
-                               placeholder="E-mail">
-                    </div>
-
-
-                    <div class="form-group row">
-                        <label for="emailFormTextBox" class="col-form-label">Email Content</label>
-                        <textarea name="emailFormTextBox" id="emailFormTextBox" cols="100" rows="14"
-                                  required></textarea>
-                    </div>
-
-
-                    <!-- Submit button -->
-                    <button class="btn light-blue text-white btn-block my-4" type="submit" name="submitEmail">Send
-                        Email
-                    </button>
-
-
-                </form>
-
-            </div>
+        <!-- Subject -->
+        <div class="send-email-row">
+            <label for="emailFormTitle" class="col-form-label">Email Subject</label>
+            <input type="text" id="emailFormTitle" name="emailFormTitle" class="form-control" placeholder="Subject" required>
         </div>
-    </div>
+
+        <!-- Full Name -->
+        <div class="send-email-row">
+            <label for="emailFormFirstName" class="col-form-label">Full Name (Optional)</label>
+            <input type="text" id="emailFormFullName" name="emailFormFullName" class="form-control" placeholder="Full Name">
+            <i class="fas fa-exclamation-circle send-email-input-warning-icon"></i> <p>Leave this blank if you do not want your name to be displayed as the sender.</p>
+        </div>
+
+        <!-- Recipients -->
+        <div class="send-email-row">
+            <label for="emailFormEmail" class="col-form-label">Recipient Email</label>
+            <input type="email" id="emailFormEmail" name="emailFormEmail" class="form-control" placeholder="E-mail">
+        </div>
+
+        <!-- Email Body -->
+        <div class="send-email-row">
+            <label for="emailFormTextBox" class="send-email-input-label">Email Content</label>
+            <textarea class="send-email-input-body" name="emailFormTextBox" id="emailFormTextBox" cols="100" rows="14" required></textarea>
+        </div>
+
+        <!-- Submit Button -->
+        <div class="send-email-row">
+            <button class="btn light-blue text-white btn-block my-4 send-email-submit-btn" type="submit" name="submitEmail">Send Email</button>
+        </div>
+
+    </form>
 
 
 <?php
