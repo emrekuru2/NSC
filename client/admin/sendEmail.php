5c6d12b3335676eed0bf6914970a3be0753d5793
macnutt <nc495758@dal.ca>
2023-02-06 23:28:55 -0400

    Removed commented-out code and unused code.

--- client/admin/sendEmail.php
+++ client/admin/sendEmail.php
@@ -1,65 +1,9 @@
 <?php
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
-
-    require '../../vendor/composer/src/Exception.php';
-    require '../../vendor/composer/src/PHPMailer.php';
-    require '../../vendor/composer/src/SMTP.php';
-
     $title = "Send Email";
-
     include_once "../includes/components/adminHeader.php";
     // Prevent Direct access and prevent non-admin's to access
     RestrictAdmin(CheckRole($_SESSION['User_ID']));
     defined('_DEFVAR') or exit(header('Location: ../index.php'));
-
-    // Swift Mailer Library
-    require_once '../../vendor/autoload.php';
-
-
-//    function sendMail($toAddress, $subject, $body, $fromName = "") {
-//        // Mail Transport
-//        echo "Sending Email...";
-//        $mail = new PHPMailer(true);
-//        // remove the following line if you want to see the debug output
-//        $mail->isSMTP();
-//        $mail->Mailer = 'smtp';
-//        $mail->SMTPDebug  = 0;
-//        $mail->SMTPAuth   = TRUE;
-//        $mail->SMTPSecure = "tls";
-//        $mail->Port       = 26;
-//        $mail->Host       = "mail.cricketnovascotia.ca";
-//        $mail->Username   = "testadmin@cricketnovascotia.ca";
-//        $mail->Password   = "CricketNSCA";
-//
-//        $mail->isHTML(true);
-//        $mail->Subject = $subject;
-//        $mail->Body    = $body;
-//        if ($fromName != "") {
-//            $mail->SetFrom("testadmin@cricketnovascotia.ca", $fromName);
-//        } else {
-//            $mail->SetFrom("testadmin@cricketnovascotia.ca", "testadmin");
-//        }
-//        $mail->AddAddress($toAddress, "toAddress");
-//        $content = $body;
-//        $mail->MsgHTML($content);
-//        if(!$mail->Send()) {
-//            echo "Error while sending Email.";
-//            var_dump($mail);
-//        } else {
-//            echo "Email sent successfully";
-//        }
-//    }
-//    if (isset($_POST['submitEmail'])) {
-//        //if they used a name to send as
-//        if (isset($_POST['emailFormFullName'])) {
-//            // send the mail with a name
-//            sendMail($_POST['emailFormEmail'],$_POST['emailFormTitle'], $_POST['emailFormTextBox'], $_POST['emailFormFullName']);
-//        } else {
-//            // send the mail without a name
-//            sendMail($_POST['emailFormEmail'],$_POST['emailFormTitle'], $_POST['emailFormTextBox']);
-//        }
-//    }
 ?>
 
     <form class="container-fluid" action="processAdminEmail.php" method="POST">
