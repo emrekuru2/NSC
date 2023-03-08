0fa33697db304effce79765d8045311a970e1bfa
Nick MacNutt <nc495758@dal.ca>
2023-03-08 14:08:06 -0400

    Removed unnecessary Email object property assignments.

--- app/Controllers/Admin/EmailController.php
+++ app/Controllers/Admin/EmailController.php
@@ -34,22 +34,13 @@ class EmailController extends BaseController
     public function sendEmail()
     { 
 
-        $to = $this->request->getVar('mailTo');
         $subject = $this->request->getVar('subject');
         $message = $this->request->getVar('message');
         $stringJSON = $this->request->getVar('groups');
         $JSON = json_decode($stringJSON);
 
         $email = \Config\Services::email();
-        $email->protocol = 'smtp';
-        $email->SMTPHost = 'mail.cricketnovascotia.ca';
-        $email->SMTPUser = 'testadmin@cricketnovascotia.ca';
-        $email->SMTPPass = 'CricketNSCA';
-        $email->SMTPPort = 26;
-        $email->SMTPCrypto = 'tls';
-        $email->BCCBatchMode = true;
-
-        $email->setTo($JSON->recipients); // TODO: Change to BCC.
+        $email->setTo($JSON->recipients);
         $email->setFrom('testadmin@cricketnovascotia.ca', 'Confirm Registration');
         $email->setSubject($subject);
         $email->setMessage($message);
