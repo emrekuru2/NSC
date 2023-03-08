9d2a120439d47808ffe189660256680e7eca79e1
Nick MacNutt <nc495758@dal.ca>
2023-03-08 17:19:30 -0400

    Fixed nsca_team table name.

--- app/Models/TeamModel.php
+++ app/Models/TeamModel.php
@@ -2,15 +2,21 @@
 
 namespace App\Models;
 
+use CodeIgniter\Database\OCI8\Builder;
 use CodeIgniter\Model;
+use CodeIgniter\Database\RawSql;
 
 class TeamModel extends Model
 {
     protected $DBGroup          = 'default';
-    protected $table            = 'nsca_team';
+    protected $table            = 'nsca_teams';
     protected $primaryKey       = 'id';
     protected $returnType       = \App\Entities\Team::class;
     protected $allowedFields    = ['name', 'description', 'image'];
+
+    function getEmails($teamID) {
+    }
+
 
     
 }
