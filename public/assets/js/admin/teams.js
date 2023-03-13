868ea46a772132ce4816f52f2a7b11faff6ad3b7
macnutt <nc495758@dal.ca>
2023-03-13 00:42:08 -0300

    Created JS for Admin Teams page.

---
+++ public/assets/js/admin/teams.js
@@ -0,0 +1,4 @@
+// Variables
+const searchBarEditButton = document.getElementById('search-bar-button')
+const searchBar = document.getElementById('search-bar')
+const allTeamEditButtons = document.getElementsByName('edit-button')
