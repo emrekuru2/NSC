f1a6d3fd03b66773e4af190da07eb7609620bbe7
yuvalmas <yuvalmas29@gmail.com>
2023-03-17 13:32:59 -0300

    changes

--- app/Controllers/Main/DevelopmentController.php
+++ app/Controllers/Main/DevelopmentController.php
@@ -9,10 +9,27 @@ class DevelopmentController extends BaseController
 {
     public function index()
     {
+<<<<<<< HEAD
         $devModel = model(DevModel::class);
 
         $data = [
             'programs'  => $devModel->programs(),
+=======
+        $db = \Config\Database::connect();
+        $builder = $db->table('nsca_dev');
+        $query = $db->query('SELECT nsca_dev.*, nsca_devprogram_type.*,
+        CASE WHEN nsca_dev_user.userID IS NULL THEN 0 ELSE 1 END AS is_registered
+        FROM nsca_dev
+        LEFT JOIN nsca_dev_user ON nsca_dev_user.devID = nsca_dev.id AND nsca_dev_user.userID = 1
+        JOIN nsca_devprogram_type ON nsca_Devprogram_type.id = nsca_dev.devProgID;');
+
+
+        $model = model(DevModel::class);
+        $data = [
+            'programs'  => 
+            $query->getResult(),
+            'pager' => $model->pager,
+>>>>>>> c9db877 (disabled the option for double registeration for dev programs)
             'title' => 'Development',
         ];
 
