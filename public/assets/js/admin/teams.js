38a33039b0c0c656524cdd4e290d248c362fe340
macnutt <nc495758@dal.ca>
2023-03-17 00:50:33 -0300

    Added JS to Admin Teams page.

--- public/assets/js/admin/teams.js
+++ public/assets/js/admin/teams.js
@@ -1,4 +1,27 @@
 // Variables
-const searchBarEditButton = document.getElementById('search-bar-button')
+const searchForm = document.getElementById('search-form')
 const searchBar = document.getElementById('search-bar')
-const allTeamEditButtons = document.getElementsByName('edit-button')
+const searchBarButton = document.getElementById('search-bar-button')
+
+const updateForm = document.getElementById('update-form')
+const updateTeamButton = document.getElementById('update-button')
+
+const deleteForm = document.getElementById('delete-form')
+const deleteTeamButton = document.getElementById('delete-button')
+
+
+// Listeners
+searchBarButton.addEventListener('click', () => {
+    if (searchBar.value !== '') searchForm.submit()
+})
+
+updateTeamButton.addEventListener('click', () => {
+    updateForm.submit()
+})
+
+deleteTeamButton.addEventListener('click', () => {
+    //deleteForm.submit()
+})
+
+
+if (searchBar.value === '') searchBarButton.disabled = true
