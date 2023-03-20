95deb9d2efcfaba8dd249ce64962ec50be01c79f
yuvalmas <yuvalmas29@gmail.com>
2023-03-20 17:03:54 -0300

    fixed dev page CSS issue

--- app/Controllers/Main/DevelopmentController.php
+++ app/Controllers/Main/DevelopmentController.php
@@ -11,14 +11,14 @@ class DevelopmentController extends BaseController
         $builder = $db->table('nsca_dev');
         // create string
         if (auth()->loggedIn()){
-            $statement = 'SELECT nsca_devs.*, nsca_dev_types.min_age, nsca_dev_types.max_age,
+            $statement = 'SELECT DISTINCT nsca_devs.*, nsca_dev_types.min_age, nsca_dev_types.max_age,
             CASE WHEN nsca_dev_users.userID IS NULL THEN 0 ELSE 1 END AS is_registered
             FROM nsca_devs';
             $statement .= ' LEFT JOIN nsca_dev_users ON nsca_dev_users.devID = nsca_devs.id AND nsca_dev_users.userID = '.auth()->id();
             $statement .= ' JOIN nsca_dev_types ON nsca_dev_types.id = nsca_devs.typeID;';
         }
         else{
-            $statement = 'SELECT nsca_devs.*, nsca_dev_types.*
+            $statement = 'SELECT nsca_devs.*, nsca_dev_types.min_age, nsca_dev_types.max_age
             FROM nsca_devs
             JOIN nsca_dev_types ON nsca_dev_types.id = nsca_devs.typeID;';
         }
