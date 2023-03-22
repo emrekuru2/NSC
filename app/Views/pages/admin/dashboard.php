66e9f258aa93edbc8b5dac8a2644ed9f7b6266ed
kuru <em434920@dal.ca>
2023-03-22 01:05:52 -0300

    Removed recent activities from dashboard

--- app/Views/pages/admin/dashboard.php
+++ app/Views/pages/admin/dashboard.php
@@ -38,51 +38,7 @@
 </div>
 <hr>
 <div class="row">
-    <div class="col-lg-6">
-        <div class="card h-100 shadow">
-            <div class="card-header">Recent Activity</div>
-            <div class="card-body">
-                <div class="timeline">
-                    <div class="tl-item active">
-                        <div class="tl-dot b-warning"></div>
-                        <div class="tl-content">
-                            <div class="">@twitter thanks for you appreciation and @google thanks for you appreciation</div>
-                            <div class="tl-date text-muted mt-1">13 june 18</div>
-                        </div>
-                    </div>
-                    <div class="tl-item">
-                        <div class="tl-dot b-primary"></div>
-                        <div class="tl-content">
-                            <div class="">Do you know how Google search works.</div>
-                            <div class="tl-date text-muted mt-1">45 minutes ago</div>
-                        </div>
-                    </div>
-                    <div class="tl-item">
-                        <div class="tl-dot b-danger"></div>
-                        <div class="tl-content">
-                            <div class="">Thanks to <a href="#" data-abc="true">@apple</a>, for iphone 7</div>
-                            <div class="tl-date text-muted mt-1">1 day ago</div>
-                        </div>
-                    </div>
-                    <div class="tl-item">
-                        <div class="tl-dot b-danger"></div>
-                        <div class="tl-content">
-                            <div class="">Order placed <a href="#" data-abc="true">@eBay</a> you will get your products</div>
-                            <div class="tl-date text-muted mt-1">1 Week ago</div>
-                        </div>
-                    </div>
-                    <div class="tl-item">
-                        <div class="tl-dot b-warning"></div>
-                        <div class="tl-content">
-                            <div class="">Learn how to use <a href="#" data-abc="true">Google Analytics</a> to discover vital information about your readers.</div>
-                            <div class="tl-date text-muted mt-1">3 days ago</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-6">
+    <div class="col-12">
         <div class="card h-100 shadow">
             <div class="card-header">Player Waitlist</div>
             <div class="card-body">
