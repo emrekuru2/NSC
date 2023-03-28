cb33cdb288a1f301e3f63d3c3e1fc90179e8f129
macnutt <nc495758@dal.ca>
2023-03-28 02:49:07 -0300

    Finished search bar design and functionality.

--- app/Views/components/search.php
+++ app/Views/components/search.php
@@ -1,9 +1,11 @@
 <?php
 /**
- * @param string $route <p>Function name of the desired route.</p>
+ * @param string $route <p>Route name of the desired function.</p>
  * @param string $name <p>Name of section (Singular). E.g., 'Team', 'Club', 'Committee', etc.</p>
  * @param array $rows <p>Array of entities from model. E.g., teams table, clubs table, etc.</p>
- * @param boolean $useName <p>Optional: Use name in search bar title. E.g., true = 'Search Teams', false = 'Search'.</p>
+ *
+ * Optional:
+ * @param boolean $useName <p>Use name in search bar title. E.g., true = 'Search Teams', false = 'Search'.</p>
  */
 ?>
 
@@ -20,7 +22,7 @@
             <tr>
                 <td class="col-11 border-style-none">
                     <div class="autocomplete">
-                        <input class="form-control form-control-sm" type="text" id="search" name="search" placeholder="Type to search <?= strtolower($name); ?>...">
+                        <input class="form-control" type="text" id="search" name="search" placeholder="Type to search <?= strtolower($name); ?>...">
                     </div>
                 </td>
             </tr>
@@ -30,6 +32,6 @@
 </form>
 
 <script>
-    let array = ['Falcons', 'Lions', 'Tigers', 'Test Team', 'Certified Bruh Moments']
+    let array = ['Falcons', 'Lions', 'Lemurs', 'Tigers', 'Test Team', 'Certified Bruh Moments']
 </script>
 <script type="text/javascript" src="<?= base_url('assets/js/search.js'); ?>"></script>
