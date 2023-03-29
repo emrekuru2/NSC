2278a432808687f30af23e316089c3435d8e740a
Connor <cn560967@dal.ca>
2023-03-28 23:25:12 -0300

    latest homepage changes

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -17,37 +17,37 @@
 
     <div class="justify-content-center container d-flex flex-wrap" style="padding-top:2vw;">
         <div id="carouselExampleIndicators" class="carousel" style="padding-right: 1vw;" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators2" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active" style=" height:15rem; width: 40vw; max-width: 100%; position: relative; overflow: hidden;">
-                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"  preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
-                <div class="carousel-caption text-end">
-                    <h1>Clubs</h1>
-                    <p>Take a look at our clubs that play in the HCL</p>
-                    <p><a class="btn btn-primary" href="/clubs">Find out more</a></p>
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators2" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active" style=" height:15rem; width: 40vw; max-width: 100%; position: relative; overflow: hidden;">
+                    <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"  preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+                    <div class="carousel-caption text-end">
+                        <h1>Clubs</h1>
+                        <p>Take a look at our clubs that play in the HCL</p>
+                        <p><a class="btn btn-primary" href="/clubs">Find out more</a></p>
+                    </div>
+                </div>
+                <div class="carousel-item" style="height: 15rem; width: 50vw; max-width: 100%; margin-right:5vh; position: relative; overflow: hidden; background-color: grey;">
+                    <img class="d-block" src="" alt="Second slide" object-fit="fill">
+                </div>
+                <div class="carousel-item" style="height: 15rem; width: 50vw; max-width: 100%; margin-right:5vh; position: relative; overflow: hidden; background-color: grey;">
+                    <img class="d-block" src="" alt="Third slide" object-fit="fill">
                 </div>
             </div>
-            <div class="carousel-item" style="height: 15rem; width: 50vw; max-width: 100%; margin-right:5vh; position: relative; overflow: hidden; background-color: grey;">
-                <img class="d-block" src="" alt="Second slide" object-fit="fill">
-            </div>
-            <div class="carousel-item" style="height: 15rem; width: 50vw; max-width: 100%; margin-right:5vh; position: relative; overflow: hidden; background-color: grey;">
-                <img class="d-block" src="" alt="Third slide" object-fit="fill">
-            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators2" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only"></span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only"></span>
+            </a>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only"></span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only"></span>
-        </a>
-        </div>
-    <!-- DevPrograms Card -->
+        <!-- DevPrograms Card -->
         <div class="card bg-info" style="height: 15rem; width: 25vw; max-width: 100%; position: relative; overflow: hidden;">
             <div class="card-body text-center pt-5" style="color: white; overflow-y: scroll;">
                 <h2 class="card-title">Development Programs</h2>
@@ -58,23 +58,22 @@
                 </p>
             </div>
         </div>
-
     </div>
 
-<!-- The New Section -->
+    <!-- The New Section -->
     <div class="container my-4">
-    <h1 class="text-center">News</h1>
-    <hr>
+        <h1 class="text-center">News</h1>
+        <hr>
     </div>
     <div class="container d-flex flex-row flex-wrap justify-content-center" style="padding-top:2vw;">
         <?php if (!empty($news) && is_array($news)) : ?>
             <?php foreach ($news as $news_item) : ?>
-                <div class="card mb-3 mx-5" style="width: 20rem;">
+                <div class="card mb-3 mx-5" style="width: 20rem; background-color: lightgray;">
                     <div class="card-body d-flex flex-column">
-                        <div class="text-center mb-3">
-                            <h4 class="card-title"><?= esc($news_item->title) ?></h4>
+                        <div class="text-center mb-1">
+                            <h3 class="card-title" style="border: #222222; background-color: black; padding: 10px; color: white"><?= esc($news_item->title) ?></h3>
+                            <?= $news_item->content ?>
                         </div>
-                        <p class="card-text overflow-auto" style="max-height: 12rem; max-width: 100%;"><?= esc($news_item->content) ?></p>
                         <div class="mt-auto text-center">
                             <a href="news/<?= esc($news_item->id)?>" class="btn btn-primary btn-block">Find out more</a>
                         </div>
@@ -86,10 +85,6 @@
             <p>Unable to find any news for you.</p>
         <?php endif ?>
     </div>
-
-
-
-
 
     <!-- The Team Section -->
     <div class="container d-flex flex-wrap justify-content-between py-5">
@@ -104,7 +99,7 @@
         </div>
         <!-- Contact Us -->
         <div class="col-lg-5 col-md-12 mb-5">
-            <div class="card h-100" style="background-color: blue;">
+            <div class="card h-100" style="background-color: deepskyblue;">
                 <div class="card-body">
                     <h3 class="card-title text-white">Contact Us</h3>
                     <div class="container text-white">
