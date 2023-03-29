1aedc58e498951e8312283a2861322d3abdc4951
Connor <cn560967@dal.ca>
2023-03-28 23:50:52 -0300

    added provincial team section

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -224,15 +224,14 @@
             <aside class="bg-primary bg-gradient rounded-3 p-4 p-sm-5 mt-5">
                 <div class="d-flex align-items-center justify-content-between flex-column flex-xl-row text-center text-xl-start">
                     <div class="mb-4 mb-xl-0">
-                        <div class="fs-3 fw-bold text-white">New products, delivered to you.</div>
-                        <div class="text-white-50">Sign up for our newsletter for the latest updates.</div>
+                        <div class="fs-3 fw-bold text-white">Provincial Team</div>
+                        <div class="text-white-50">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+                            <br> Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec.</div>
                     </div>
                     <div class="ms-xl-4">
                         <div class="input-group mb-2">
-                            <input class="form-control" type="text" placeholder="Email address..." aria-label="Email address..." aria-describedby="button-newsletter" />
-                            <button class="btn btn-outline-light" id="button-newsletter" type="button">Sign up</button>
+                            <button class="btn btn-outline-light" id="button-newsletter" type="button">Find out More</button>
                         </div>
-                        <div class="small text-white-50">We care about privacy, and will never share your data.</div>
                     </div>
                 </div>
             </aside>
