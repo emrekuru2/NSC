9af52159f1c0db2b9e5a2ffdacf007e3b7058cd9
macnutt <nc495758@dal.ca>
2023-03-30 17:34:35 -0300

    Added club team list and unassigned teams list to Admin Clubs page, and created functions in TeamModel

--- app/Models/TeamModel.php
+++ app/Models/TeamModel.php
@@ -10,4 +10,21 @@ class TeamModel extends Model
     protected $primaryKey       = 'id';
     protected $returnType       = \App\Entities\Team::class;
     protected $allowedFields    = ['id', 'clubID', 'name', 'description', 'image'];
+
+    // Functions
+    public function getTeamsInClub(int $clubID): array
+    {
+        return $this->select('nsca_teams.id, nsca_teams.name')
+            ->where('nsca_teams.clubID', $clubID)
+            ->orderBy('nsca_teams.name', 'ASC')
+            ->findAll();
+    }
+
+    public function getUnassignedTeams(): array
+    {
+        return $this->select('nsca_teams.id, nsca_teams.name')
+            ->where('nsca_teams.clubID')
+            ->orderBy('nsca_teams.name', 'ASC')
+            ->findAll();
+    }
 }
