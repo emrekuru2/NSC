84a389fc96f1166e99df17cae9e332ba1b4f92c5
macnutt <nc495758@dal.ca>
2023-04-01 17:01:03 -0300

    Added phone number listener to add dashes when user forgets.

--- public/assets/js/admin/clubs.js
+++ public/assets/js/admin/clubs.js
@@ -74,6 +74,17 @@ addTeamsButton.addEventListener('click', () => {
     addTeamsForm.submit()
 })
 
+document.querySelectorAll('input[type=tel]').forEach((input) => {
+    input.addEventListener('change', () => {
+        let phone = input.value
+        if (phone.length === 10 && (phone.match(new RegExp("/-/", "g")) || []).length === 0 ||
+            phone.length === 12 && (phone.match(new RegExp("/-/", "g")) || []).length === 1)
+        {
+            input.value = phone.substring(0, 3) + '-' + phone.substring(3, 6) + '-' + phone.substring(6, 10)
+        }
+    })
+})
+
 // Functions
 function getClubMemberRolesJSON() {
     if (clubMemberTableBody.dataset.clubIsset === '0' && clubMemberTableBody.children[0].children[0].innerText === 'No club members') return ''
