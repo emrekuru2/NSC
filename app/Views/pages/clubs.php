1d70f372b94abe1352f71e07aeed26a2b9c2bc4e
Natalie Hargreaves <nt479065@dal.ca>
2023-04-04 13:39:43 -0300

    modals dynamically change from json

--- app/Views/pages/clubs.php
+++ app/Views/pages/clubs.php
@@ -19,8 +19,8 @@
                         <div class="card h-100 shadow border-0">
                             <img class="card-img-top" src="<?=base_url("/assets/images/Clubs/default.png")?>" alt="..." />
                             <div class="card-body p-4">
-                                <input type="hidden" name="club-json" value="{ 'name':'<?=$club->name?>', 'email':'<?=$club->email?>', 'description':'<?=$club->description?>', 'website':'<?=$club->website?>', 'phone':'<?=$club->phone?>', 'facebook':'<?=$club->facebook?>' }" name="clubJSON">
-                                <button type="button" name="club-button" id="<?=$club->id?>" class="btn btn-primary" ><?=$club->name?></button>                                
+                                <input type="hidden" name="club-json" value='{"cName":"<?=$club->name?>", "email":"<?=$club->email?>", "description":"<?=$club->description?>", "website":"<?=$club->website?>", "phone":"<?=$club->phone?>", "facebook":"<?=$club->facebook?>"}'>
+                                <button type="button" name="club-button" id="<?=$club->id?>" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#clubsModal"><?=$club->name?></button>                                
                             </div>
                         </div> 
                     </div>
@@ -36,15 +36,14 @@
                     <div class="modal-dialog">
                         <div class="modal-content">
                         <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="modal-header"><?= esc($clubs[1]->name) ?></h1>
+                            <h1 class="modal-title fs-5" id="modal-header"></h1>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            <p><?= esc($clubs[1]->email) ?></p>
+                            <p id="modal-email"></p>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
                         </div>
                         </div>
                     </div>
@@ -63,14 +62,21 @@
             let clubJsons = document.getElementsByName('club-json');
 
             console.log(clubInfoButtons)
+            //issue with parsing
+            //console.log(clubJsons[0].value)
+            //let json = JSON.parse(clubJsons[0].value);
+            //console.log(json.cName)
 
             for (let i = 0; i < clubInfoButtons.length; i++) {
-                clubInfoButtons[i].addEventListener('click', (event) => {
-                    let json = JSON.parse(clubJsons[i]);
-                    console.log(json.name)
-                    document.getElementById('model-header').innerText = json.name
-                    
-                })
+                clubInfoButtons[i].addEventListener('click', runModal.bind(this, i));
+            }
+
+            function runModal(j) {
+                let json = JSON.parse(clubJsons[j].value);
+                console.log(json.cName)
+                //innertext getting overwritten with each loop?
+                document.getElementById('modal-header').innerText = json.cName
+                document.getElementById('modal-email').innerText = json.email
             }
         </script>
 
