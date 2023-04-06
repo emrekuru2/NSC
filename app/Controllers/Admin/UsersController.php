a6af842d8a41ed92191dede8501d4332fb26f8f8
yuvalmas <yuvalmas29@gmail.com>
2023-04-06 11:26:24 -0300

    fixed issue with users

--- app/Controllers/Admin/UsersController.php
+++ app/Controllers/Admin/UsersController.php
@@ -59,58 +59,58 @@ class UsersController extends BaseController
         return view('pages/admin/users', $data);
     }
 
-    // public function userDetails(int $id)
-    // {
-    //     $data = [
-    //         'title' => 'User Editing',
-    //         'user'  => model(UserModel::class)->find($id),
-    //         'teams' => model(TeamModel::class)->findAll(),
-    //         'clubs' => model(ClubModel::class)->findAll(),
-    //     ];
+    public function userDetails(int $id)
+    {
+        $data = [
+            'title' => 'User Editing',
+            'user'  => model(UserModel::class)->find($id),
+            'teams' => model(TeamModel::class)->findAll(),
+            'clubs' => model(ClubModel::class)->findAll(),
+        ];
 
-    //     return view('pages/admin/edit_user', $data);
-    // }
+        return view('pages/admin/edit_user', $data);
+    }
 
-    // public function searchUserDetails()
-    // {
-    //     $name = esc($this->request->getVar('search'));
+    public function searchUserDetails()
+    {
+        $name = esc($this->request->getVar('search'));
 
-    //     $firstname = explode(' ', $name)[0];
-    //     $user      = model(UserModel::class)->where('first_name', $firstname)->first();
+        $firstname = explode(' ', $name)[0];
+        $user      = model(UserModel::class)->where('first_name', $firstname)->first();
 
-    //     if ($user === null) {
-    //         return redirect()->back();
-    //     }
+        if ($user === null) {
+            return redirect()->back();
+        }
 
-    //     $data = [
-    //         'title' => 'User Editing',
-    //         'user'  => $user,
-    //         'teams' => model(TeamModel::class)->findAll(),
-    //         'clubs' => model(ClubModel::class)->findAll(),
-    //     ];
+        $data = [
+            'title' => 'User Editing',
+            'user'  => $user,
+            'teams' => model(TeamModel::class)->findAll(),
+            'clubs' => model(ClubModel::class)->findAll(),
+        ];
 
-    //     return view('pages/admin/edit_user', $data);
-    // }
+        return view('pages/admin/edit_user', $data);
+    }
 
-    // public function editUser(int $id)
-    // {
-    //     $data = $this->request->getPost();
+    public function editUser(int $id)
+    {
+        $data = $this->request->getPost();
 
-    //     if (isset($data['role'])) {
-    //         $row = model(PermissionModel::class)->where('user_id', $id)->first();
-    //         model(PermissionModel::class)->update($row['id'], ['permission' => $data['role']]);
-    //     }
+        if (isset($data['role'])) {
+            $row = model(PermissionModel::class)->where('user_id', $id)->first();
+            model(PermissionModel::class)->update($row['id'], ['permission' => $data['role']]);
+        }
 
-    //     if (isset($data['team'])) {
-    //         $row = model(TeamUserModel::class)->where('userID', $id);
-    //         model(TeamUserModel::class)->update($row->id, ['teamID' => $data['team']]);
-    //     }
+        if (isset($data['team'])) {
+            $row = model(TeamUserModel::class)->where('userID', $id);
+            model(TeamUserModel::class)->update($row->id, ['teamID' => $data['team']]);
+        }
 
-    //     if (isset($data['club'])) {
-    //         $row = model(ClubUserModel::class)->where('userID', $id);
-    //         model(ClubUserModel::class)->update($row->id, ['clubID' => $data['club']]);
-    //     }
+        if (isset($data['club'])) {
+            $row = model(ClubUserModel::class)->where('userID', $id);
+            model(ClubUserModel::class)->update($row->id, ['clubID' => $data['club']]);
+        }
 
-    //     return redirect()->back();
-    // }
+        return redirect()->back();
+    }
 }
