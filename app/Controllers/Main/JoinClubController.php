20324ba3a0312ca2f4af5ec567a0e36dff6edcee
macnutt <nc495758@dal.ca>
2023-04-10 23:59:04 -0300

    Fixed join club bug inside JoinClubController.php where submitting a request for the same team threw an exception.

--- app/Controllers/Main/JoinClubController.php
+++ app/Controllers/Main/JoinClubController.php
@@ -5,6 +5,7 @@ namespace App\Controllers\Main;
 use App\Controllers\BaseController;
 use App\Models\ClubJoinlistModel;
 use App\Models\ClubModel;
+use Exception;
 
 class JoinClubController extends BaseController
 {
@@ -33,46 +34,64 @@ class JoinClubController extends BaseController
     }
 
     public function joinClub(){
-        $clubJoinistModel = model(ClubJoinlistModel::class);
+        $clubJoinListModel = model(ClubJoinlistModel::class);
         $clubModel = model(ClubModel::class);
 
         // checking if the user submitted a request before
-        $checkUser = $clubJoinistModel->select()->where('userID', auth()->id())->first();
+        $checkUser = $clubJoinListModel->select()->where('userID', auth()->id())->first();
 
-        if($checkUser != NULL){
-            // updating the user's request
+        if($checkUser != null){
             $checkUser->clubID = $clubModel->select()->where('name', $_POST['clubs-select'])->first()->id;
-            if ($clubJoinistModel->save($checkUser)) {
+
+            try {
+                if ($clubJoinListModel->save($checkUser)) {
+                    $data = [
+                        'type'    => 'success',
+                        'content' => 'Your request was submitted successfully!'
+                    ];
+                } else {
+                    $data = [
+                        'type'    => 'danger',
+                        'content' => 'Error while submitting your request. Please try again.'
+                    ];
+                }
+            } catch (Exception $e) {
                 $data = [
                     'type'    => 'success',
-                    'content' => 'Your request was updated successfully'
-                ];
-            } else {
-                $data = [
-                    'type'    => 'danger',
-                    'content' => 'Failed to submit your request'
+                    'content' => 'Request has already been submitted.'
                 ];
             }
 
         } else {
-            // adding the user request
-            $currentUser = new \App\Entities\UserTypes\ClubUser();
-            $currentUser->userID = auth()->id();
             $clubID = $clubModel->select()->where('name', $_POST['clubs-select'])->first()->id;
-            $currentUser->clubID = $clubID;
 
-            // saving the current (new) user
-            if ($clubJoinistModel->save($currentUser)) {
+            $data = [
+                'userID' => auth()->id(),
+                'clubID' => $clubID
+            ];
+
+            $newRequest = new \App\Entities\UserTypes\ClubUser();
+            $newRequest->fill($data);
+
+            try {
+                if ($clubJoinListModel->save($newRequest)) {
+                    $data = [
+                        'type'    => 'success',
+                        'content' => 'Your request was submitted successfully'
+                    ];
+                } else {
+                    $data = [
+                        'type'    => 'danger',
+                        'content' => 'Failed to submit your request'
+                    ];
+                }
+            } catch (Exception $e) {
                 $data = [
                     'type'    => 'success',
-                    'content' => 'Your request was submitted successfully'
-                ];
-            } else {
-                $data = [
-                    'type'    => 'danger',
-                    'content' => 'Failed to submit your request'
+                    'content' => 'Request has already been submitted.'
                 ];
             }
+
         }
 
         return redirect()->back()->with('alert', $data);
@@ -80,10 +99,10 @@ class JoinClubController extends BaseController
 
 
     public function deleteRequest(){
-        $clubJoinistModel = model(ClubJoinlistModel::class);
-        $previousRequestID = $clubJoinistModel->select()->where('userID', auth()->id())->first()->id;
+        $clubJoinListModel = model(ClubJoinlistModel::class);
+        $previousRequestID = $clubJoinListModel->select()->where('userID', auth()->id())->first()->id;
 
-        if($clubJoinistModel->delete($previousRequestID)){
+        if($clubJoinListModel->delete($previousRequestID)){
             $data = [
                 'type'    => 'success',
                 'content' => 'Your request was deleted successfully'
