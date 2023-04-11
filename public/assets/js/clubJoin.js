8e3427c456ebfdf78d5757779fc43d58c24eab94
macnutt <nc495758@dal.ca>
2023-04-10 22:50:46 -0300

    Refactored clubJoin.js.

--- public/assets/js/clubJoin.js
+++ public/assets/js/clubJoin.js
@@ -1,28 +1,22 @@
+// Variables
 const submitBtn = document.getElementById('submit');
 const modalMessage = document.getElementById('modal-message');
 const clubsSelect = document.getElementById('clubs-select');
 
+const submitButton = document.getElementById("submit");
+const selectDropdown = document.getElementById("clubs-select");
+
+
+// Listeners
 submitBtn.addEventListener('click', () => {
   const selectedOption = clubsSelect.options[clubsSelect.selectedIndex];
   modalMessage.textContent = selectedOption.value;
 });
 
-
-
-const submitButton = document.getElementById("submit");
-const selectDropdown = document.getElementById("clubs-select");
-
-// Disable submit button by default
-submitButton.disabled = true;
-
-// Listen for changes on the dropdown
 selectDropdown.addEventListener("change", function() {
-    // If a value is selected, enable the submit button
-    if (selectDropdown.value !== "0") {
-        submitButton.disabled = false;
-    } else {
-        // Otherwise, disable the submit button
-        submitButton.disabled = true;
-    }
+    submitButton.disabled = selectDropdown.value === "0";
 });
 
+
+// Main
+submitButton.disabled = true;
