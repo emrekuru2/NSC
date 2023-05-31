fcae12bb208cfca46fba9d927192d39fd69920b3
Kirtan <kr327739@dal.ca>
2023-05-31 12:55:32 -0300

    Development page updated by showing the list

--- app/Views/pages/development.php
+++ app/Views/pages/development.php
@@ -13,50 +13,76 @@ use CodeIgniter\I18n\Time;
         <h1 class="display-3 text-center font-weight-bold">Development Programs</h1>
         <br>
 
-        <?php if (! empty($programs) && is_array($programs)) : ?>
-            <?php foreach ($programs as $program) : ?>
-                <?php if ($program->start_date < Time::now()):?>
-                    <div class="card my-3">
+        <style>
+          
+          .program-details {
+            display: none;
+          }
 
-                    <div class="card-header d-flex">
-                        <?= esc($program->name) ?>
+          .show-details {
+            display: block;
+          }
+        </style>
 
-                    </div>
-                    <div class="card-body">
-                        <div class="d-flex justify-content-between">
-                            <div>
-                                <p><b>General Information</b></p>
-                                <p><b>Days</b>: <?= esc($program->daysRun) ?></p>
-                                <p><b>Dates:</b> <?= date('m/d/y', strtotime(esc($program->start_date))) ?>-<?= date('m/d/y', strtotime(esc($program->end_date)))?></p>
-                                <p><b>Time:</b> <?= date('H:m', strtotime(esc($program->start_time))) ?>-<?= date('H:m', strtotime(esc($program->end_time)))?></p>
-                                <p><b>Ages:</b> <?= esc($program->min_age) ?>-<?= esc($program->max_age) ?></p>
-                                <p><b>Location:</b> <?= esc($program->location) ?></p>
-                                <p><b>Cost:</b> $<?= esc($program->price) ?></p>
+<?php if (!empty($programs) && is_array($programs)) : ?>
 
-                            </div>
-                            <img src="<?= base_url($program->image); ?>"></img>
-                        </div>
-                        <hr>
-                        <p><b>Description:</b></p>
-                        <p><?= esc($program->description) ?></p>
 
-                        <?php if ((auth()->loggedIn()) && esc($program->is_registered) === 0):?>
-                            <a href="/development/<?=esc($program->id)?>" class="btn btn-primary">Register</a>
-                        <?php elseif ((auth()->loggedIn()) && esc($program->is_registered) === 1):?>
-                            <a class="btn btn-secondary">Already registered</a>
-                        <?php else: ?>
-                            <a href="/login" class="btn btn-primary">Login to Register</a>
-                        <?php endif ?>
-                    <?php endif ?>
+<ul>
 
-                    </div>
+    <?php foreach ($programs as $program) : ?>
 
-                </div>
+      <?php if ($program->start_date < Time::now()) : ?>
+
+        <li class="program-item">
+
+          <button class="program-button"><?= esc($program->name) ?></button>
+          <div class="program-details">
+
+            <p><b>General Information</b></p>
+            <p><b>Days</b>: <?= esc($program->daysRun) ?></p>
+            <p><b>Dates:</b> <?= date('m/d/y', strtotime(esc($program->start_date))) ?>-<?= date('m/d/y', strtotime(esc($program->end_date))) ?></p>
+            <p><b>Time:</b> <?= date('H:m', strtotime(esc($program->start_time))) ?>-<?= date('H:m', strtotime(esc($program->end_time))) ?></p>
+            <p><b>Ages:</b> <?= esc($program->min_age) ?>-<?= esc($program->max_age) ?></p>
+            <p><b>Location:</b> <?= esc($program->location) ?></p>
+            <p><b>Cost:</b> $<?= esc($program->price) ?></p>
+            <p><b>Description:</b></p>
+            <p><?= esc($program->description) ?></p>
+
+            <?php if ((auth()->loggedIn()) && esc($program->is_registered) === 0) : ?>
+              <a href="/development/<?= esc($program->id) ?>" class="btn btn-primary">Register</a>
+            <?php elseif ((auth()->loggedIn()) && esc($program->is_registered) === 1) : ?>
+              <a class="btn btn-secondary">Already registered</a>
+            <?php else : ?>
+              <a href="/login" class="btn btn-primary">Login to Register</a>
+            <?php endif ?>
+
+          </div>
+
+        </li>
+
+        <?php endif ?>
         <?php endforeach ?>
-    <?php else : ?>
-        <h3>No Programs</h3>
-        <p>Unable to find any programs for you.</p>
-    <?php endif ?>
+</ul>
+
+  <script>
+    document.addEventListener('DOMContentLoaded', function() {
+      var programButtons = document.querySelectorAll('.program-button');
+
+      programButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+          var programDetails = this.nextElementSibling;
+          programDetails.classList.toggle('show-details');
+        });
+      });
+    });
+  </script>
+
+
+<?php else : ?>
+  <h3>No Programs</h3>
+  <p>Unable to find any programs for you.</p>
+<?php endif ?>
+
 </div>
 
 <?= $this->endSection() ?>
