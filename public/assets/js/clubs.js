46972224fa1e6eda017b735fd360aafaf76d9e1f
mansi <mn490901@dal.ca>
2023-05-31 12:51:11 -0300

    Club details for showing teams in the pop up option

--- public/assets/js/clubs.js
+++ public/assets/js/clubs.js
@@ -6,7 +6,7 @@ for (let i = 0; i < clubInfoButtons.length; i++) {
     clubInfoButtons[i].addEventListener('click', runModal.bind(this, i));
 }
 
-function runModal(j) {
+function runModal(j, buttonId) {
     let json = JSON.parse(clubJsons[j].value);
     document.getElementById('modal-header').innerText = json.cName;
 
@@ -36,13 +36,5 @@ function runModal(j) {
     } else {
         document.getElementById('modal-fb').innerText = 'N/A'
         document.getElementById('modal-fb').removeAttribute('href')
-    }
-    
-    let teamInfoButtons = document.getElementsByName('team-button');
-    let teamJsons = document.getElementsByName('team-json');
-
-    for (let i = 0; i < teamInfoButtons.length; i++) {
-        //adds click event listener to team bottons
-        teamInfoButtons[i].addEventListener('click', runModal.bind(this, i));
     }
 }
