054a153b2ea4c42a8e1229252994f4e159087393
keyush patel <ky280311@dal.ca>
2023-07-24 11:17:51 -0300

    resolved the merge conflict created due deletion of libraries folder

--- app/Cells/ModalCell.php
+++ app/Cells/ModalCell.php
@@ -12,6 +12,7 @@ class ModalCell extends Cell
     public $currentClub;
     public $selection;
     public $program;
+    public $committee;
     protected string $view;
 
     public function mount(?string $type)
@@ -28,6 +29,8 @@ class ModalCell extends Cell
                 return 'views/modals/clubModal';
             case 'dev':
                 return 'views/modals/devModal';
+            case  'committee':
+                return 'views/modals/committeeModal';
         }
     }
 }
--- app/Controllers/Main/CommitteesController.php
+++ app/Controllers/Main/CommitteesController.php
@@ -18,35 +18,4 @@ class CommitteesController extends BaseController
         return view('pages/main/committees', $data);
     }
     
-    public function view($id)
-
-    {
-
-        $model = model(CommitteeModel::class);
-
-        $committee = $model->find($id);
-
-        if ($committee === null) {
-
-            // Committee not found, handle the error appropriately
-
-            return $this->response->setStatusCode(404)->setJSON(['error' => 'Committee not found.']);
-
-        }
-
-        $members = $model->getMembers($id);
-
-        $data = [
-
-            'committee' => $committee,
-
-            'members'   => $members,
-
-            'title'     => 'Committee Details',
-
-        ];
-
-        return $this->response->setJSON($data);
-
-    }
 }
